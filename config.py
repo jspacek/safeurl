2dafefff99efddb5164acd7601113da948d0735d
jspacek <jodi.spacek@gmail.com>
2018-11-06 20:17:40 -0800

    Revert "Load balancing in code using 2-choice randomized algorithm"
    This reverts commit 671e71c50753b3ae7520f559007c12910738820c.

--- config.py
+++
@@ -1,5 +0,0 @@
-PROD = {'is_prod': True}
-DB = {'db_prod_master':'postgres://anotherlevel:K!tten123@safeurl.chpmbkuhjhuc.us-east-1.rds.amazonaws.com/safeurl',
-    'db_prod_slave1':'postgres://anotherlevel:K!tten123@safeurl1.chpmbkuhjhuc.us-east-1.rds.amazonaws.com/anotherlevel',
-    'db_prod_slave2':'postgres://anotherlevel:K!tten123@safeurl2.chpmbkuhjhuc.us-east-1.rds.amazonaws.com/safeurl2',
-    'db_dev':'sqlite:///safeurl.db'}
--- core/util.py
+++ core/util.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python3
 import hashlib
 from urllib.parse import urlparse
-from random import randint
 
 """
 Utility methods for hashing, searching, and parsing URLs
-Load balancing algorithm
 """
 def parse_domain(raw_url):
     if (raw_url == None):
@@ -27,22 +25,3 @@ def in_domain(rs, hash_url):
         if(domain.hash_url == hash_url):
             return True
     return False
-
-def two_choice_load_balance(db_strings, usages):
-    # 2-choice randomized load balancing algorithm maximum load bound is O(log log n/log 2) + theta(1)
-    # (See Mitzenmacher d-choice randomized selection, where d=2)
-
-    # 1) Select two DB instances randomly
-    instance1 = randint(0, len(usages)-1)
-    instance2 = randint(0, len(usages)-1)
-
-    # 2) Choose the lighter loaded proxy and increment usage
-    if (usages[instance1] <= usages[instance2]):
-        db_string = db_strings[instance1]
-        usages[instance1] = usages[instance1] + 1
-    else:
-        db_string = db_strings[instance2]
-        usages[instance2] = usages[instance2] + 1
-    #print(usages)
-
-    return db_string
