23ab6a22bc32ed02d4fc541c255c715a92f3417d
jspacek <jodi.spacek@gmail.com>
2018-11-06 20:21:07 -0800

    Adding configuration

---
+++ config.py
@@ -0,0 +1,5 @@
+PROD = {'is_prod': True}
+DB = {'db_prod_master':'postgres://anotherlevel:password@safeurl.chpmbkuhjhuc.us-east-1.rds.amazonaws.com/safeurl',
+    'db_prod_slave1':'postgres://anotherlevel:password@safeurl1.chpmbkuhjhuc.us-east-1.rds.amazonaws.com/anotherlevel',
+    'db_prod_slave2':'postgres://anotherlevel:password@safeurl2.chpmbkuhjhuc.us-east-1.rds.amazonaws.com/safeurl2',
+    'db_dev':'sqlite:///safeurl.db'}
